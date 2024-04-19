cfa2c7fbcbe6db5d1c6d0d569eb83058c13d35dc
Diego Romero <diego.romero.s@hotmail.com>
2024-04-19 02:24:41 -0400

    Add light in power action

--- src/components/newPs1/ps1v2.jsx
+++ src/components/newPs1/ps1v2.jsx
@@ -1,36 +1,39 @@
-import React from "react"
+import React from "react";
 import "../ps1/ps1.css";
-import { useState,useEffect } from 'react';
+import { useState } from "react";
 export const Ps1_2 = () => {
   const [animate, setAnimate] = useState("outer");
+  const [power, setPower] = useState("span");
 
-  const handleClick  = () => {
-    setAnimate("outerPlusAnimation");
+  const handleClick = () => {
+    setPower("span2");
+    setTimeout(() => {
+      setAnimate("outerPlusAnimation");
+    }, 2000);
     
-  }
+    
+  };
 
   return (
     <div>
-      {/*<button onClick={handleClick}>dsad</button>*/}
+      {/**/}
+      <button onClick={handleClick}>dsad</button>
       <div className={animate}>
-      <div className="ps1">
-        <div className="left">
-          <div className="reset">RESET</div>
-          <div className="power">POWER</div>
-          <span></span>
+        <div className="ps1">
+          <div className="left">
+            <div className="reset">RESET</div>
+            <div className="power">POWER</div>
+            <span className={power}></span>
+          </div>
+          <div className="center">
+            <span></span>
+          </div>
+          <div className="right">
+            <div className="open">OPEN</div>
+            <span></span>
+          </div>
         </div>
-        <div className="center">
-          <span></span>
-        </div>
-        <div className="right">
-        <div className="open">OPEN</div>
-        <span></span>
-        </div>
-      
       </div>
-      </div>
-
-
     </div>
   );
-}
+};
