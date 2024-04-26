c7e38945cf7e28af61d4a3e4eb83c5a4b7fd1534
Diego Romero <diego.romero.s@hotmail.com>
2024-04-26 01:48:40 -0400

    add no read disc image

---
+++ src/components/secondPart/SecondPart.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function SecondPart() {
+  return (
+    <div className='main-layer'>
+      <div className='mainMenu_Box'><p>MAIN MENU</p></div>
+
+      
+      <div className='layerBalls'>
+      <div className='upperBalls'>
+        <div className='firstBall'></div>
+        <div className='firstBall'></div>
+        </div>
+        <div className='lowerBalls'>
+        <div className='firstBall'></div>
+        <div className='firstBall'></div>
+        </div>
+      </div>
+      <div className='MemoryAndPlayer'>
+        <p>MEMORY CARD</p>
+      <div className='colorButton'></div>
+      <p>CD PLAYER</p>
+      <div className='colorButton'></div>
+      </div>
+    
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SecondPart from './components/secondPart/SecondPart';
+import '../src/components/secondPart/SecondPart.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <SecondPart/>
   </React.StrictMode>
 );
 
