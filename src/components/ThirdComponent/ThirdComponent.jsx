8f401783584f812e58be8256e87719714efaa3a2
Diego Romero <diego.romero.s@hotmail.com>
2024-05-08 03:34:33 -0400

    add props to choose video

--- src/components/ThirdComponent/ThirdComponent.jsx
+++ src/components/ThirdComponent/ThirdComponent.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-export default function ThirdComponent() {
-    const videoId = "e4RsLBCNbrU"; // El ID del video de YouTube
 
+export default function ThirdComponent({ videoId = 0}) {
+  const videos = [
+    { id: "e4RsLBCNbrU", title: "Video 1" },
+    { id: "another-video-id", title: "Video 2" },
+    // Agregar más videos según sea necesario
+  ];
+  const videoIndex = videoId != null ? videoId : 0;
+  
+  // Obtener el video correspondiente al índice
+  const selectedVideo = videos[videoIndex];
+
+  if (!selectedVideo) {
+    return <p>Video no encontrado</p>;
+  }
   return (
 
     <div className="tvsimulator2">
@@ -10,7 +22,7 @@ export default function ThirdComponent() {
       <iframe 
         width="460"
         height="315"
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&mute=1`}
+        src={`https://www.youtube.com/embed/${selectedVideo.id}?autoplay=1&controls=0&mute=1`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       ></iframe>
