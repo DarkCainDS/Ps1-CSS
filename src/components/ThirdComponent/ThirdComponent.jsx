3ed30741c05ecbd9095195b6751893e83dca71af
Diego Romero <diego.romero.s@hotmail.com>
2024-05-09 03:15:22 -0400

    add some urls

--- src/components/ThirdComponent/ThirdComponent.jsx
+++ src/components/ThirdComponent/ThirdComponent.jsx
@@ -3,8 +3,17 @@ import React from "react";
 
 export default function ThirdComponent({ videoId = 0}) {
   const videos = [
-    { id: "e4RsLBCNbrU", title: "Video 1" },
-    { id: "another-video-id", title: "Video 2" },
+    { id: "e4RsLBCNbrU", title: "Megaman X4" },
+    { id: "HoG9jmXMdW0", title: "Tekken 3" },
+    { id: "x6ivwknQN54", title: "Medal of Honor" },
+    { id: "rEHQRDnyUgU", title: "Brave Fencer Musashi" },
+    { id: "nCVjlcNByqg", title: "DBZ Final Bout" },
+    { id: "WajTS0JSvOo", title: "RE 3" },
+    { id: "xX4FWQVBr80", title: "Dragon Valor" },
+    { id: "VFXXkhvCLMc", title: "FF 7" },
+    { id: "0uogEkbAufg", title: "Dino Crisis 2" },
+    
+    
     // Agregar más videos según sea necesario
   ];
   const videoIndex = videoId != null ? videoId : 0;
