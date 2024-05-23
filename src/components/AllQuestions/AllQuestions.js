bfee78929184931e4e5d4309c8f1d463849ab148
Diego Romero <diego.romero.s@hotmail.com>
2024-05-23 01:14:37 -0400

    More questions about analisis

---
+++ src/components/AllQuestions/AllQuestions.js
@@ -0,0 +1,4 @@
+const AllQuestions = [
+
+  ];
+  export default AllQuestions
---
+++ src/components/AllQuestions/AnalisisExploratorio.js
@@ -0,0 +1,268 @@
+const AnalisisExploratorio = [
+    {
+      questionTitle: 'Completa el siguiente enunciado: La analitica descriptiva y diagnostica realizan preguntas sobre el _____, mientras el analitica predictiva y descriptiva realizan preguntas sobre el  _____',
+      options: [
+        { answerText: '', isCorrect: false },
+        { answerText: '', isCorrect: false },
+        { answerText: 'pasado, futuro', isCorrect: true },
+        { answerText: '', isCorrect: false },
+      ],
+    },
+    {
+        questionTitle: 'En que orden de complejidad y tiempo se encuentra los distintos tipos de analitica de datos?',
+        options: [
+          { answerText: 'Descriptiva, Diagnostica,Predictiva Prescriptiva', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: 'Las variables ordinales y nominales pertenecen a las variables tipo ____',
+        options: [
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: 'Categorico', isCorrect: true },
+        ],
+      },
+      {
+        questionTitle: '¿Puedes Realizar analisis exploratorio de datos en la nube?',
+        options: [
+          { answerText: 'Falso', isCorrect: false },
+          { answerText: 'Verdadero', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '¿ Que busca analizar el analisis univariado ?',
+        options: [
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: 'Se centra en el estudio de una sola variable a la vez, examinando sus características y distribuciones', isCorrect: true },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '¿Cual de las siguientes listas de elementos no debe ser validada al momento de trabajar ocn datos?',
+        options: [
+          { answerText: 'Todas deben ser validadas', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '¿ Cual es una consecuencia de no limpar y validar nuestros datos?',
+        options: [
+          { answerText: 'Si los datos no son precisos desde el comienzo, los resultados definitivamente no seran precisos', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '¿ Cual de los siguientes comandos puede ayudarte a conocer cuantas variaables de cada tipo existen en tus datos?',
+        options: [
+          { answerText: '', isCorrect: false },
+          { answerText: 'df.types.value_counts()', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: 'De tener valores faltantes, ¿ que comando te permite cuantificar cuantos existen por cada variable ?',
+        options: [
+          { answerText: '', isCorrect: false },
+          { answerText: 'df.isnull().sum().sum()', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: 'df.isnull().sum() o df.isna().sum()', isCorrect: true },
+        ],
+      },
+      {
+        questionTitle: 'Para eliminar los valores faltantes de tu conjuntode datos ¿que comando podria utilizar?',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      {
+        questionTitle: '',
+        options: [
+          { answerText: '', isCorrect: true },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+          { answerText: '', isCorrect: false },
+        ],
+      },
+      
+  ];
+  export default AnalisisExploratorio
